167cb8d0f9b5bbcd97c957e0c73c323685007f85
ROOT\sponomarev <sponomarev@intermedia.com>
2022-01-18 12:46:54 +0400

    user input logic added

--- main.go
+++ main.go
@@ -7,16 +7,28 @@ import (
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
+	var remainingTickets uint = conferenceTickets
 
 	fmt.Printf("Welcome to our %v booking application \n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	var userName string
+	var userLastName string
+	var userEmail string
+	var userTickets int
 
-	//ask user their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&userName)
 
-	userName = "Tom"
-	fmt.Println(userName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&userLastName)
+
+	fmt.Println("Enter your last email: ")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, userLastName, userTickets, userEmail)
 }
